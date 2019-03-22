81992daf4aa0bf67173c0d4b23e7436ea053a979
VictorSantoryu <victor.santoryu@github.com>
2019-03-21 21:36:59 -0300

    Atualiza ao iniciar e botão

--- BitcoinPreco/ViewController.swift
+++ BitcoinPreco/ViewController.swift
@@ -9,12 +9,73 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var precoBiticoins: UILabel!
+    
+    @IBOutlet weak var botaoAtualizar: UIButton!
+    
+    @IBAction func atualizarPreco(_ sender: Any) {
+        self.recuperaPrecoBitCoin()
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.recuperaPrecoBitCoin()
+
+    }
+    
+    func formaratPReco(preco: NSNumber)-> String {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.locale = Locale(identifier: "pt_BR")
+        
+        if let precoFinal = nf.string(from: preco){
+            return precoFinal
+        }
+        return "0,00"
     }
 
+    func recuperaPrecoBitCoin(){
+        
+        self.botaoAtualizar.setTitle("Atualizando...", for: .normal)
+        
+        if let url = URL(string: "https://blockchain.info/pt/ticker"){
+            let tarefa = URLSession.shared.dataTask(with: url) { (dados, requisicao, erro) in
+                
+                if erro == nil{
+                    
+                    if let dadosRetorno = dados {
+                        
+                        do{
+                            let objetoJson = try JSONSerialization.jsonObject(with: dadosRetorno, options: []) as? [String: Any]
+                            
+                            if let brl = objetoJson?["BRL"] as? [String: Any]{
+                                if let preco = brl["last"] as? Double {
+                                    let precoFormatado = self.formaratPReco(preco: NSNumber(value: preco))
+                                    
+                                    DispatchQueue.main.async(execute: {
+                                        self.precoBiticoins.text = "R$ \(precoFormatado)"
+                                        self.botaoAtualizar.setTitle("Atualizar", for: .normal)
+                                    })
+                                }
+                            }
+                            
+                        }catch{
+                            print("Erro ao formatar Retorno ou seja converter")
+                        }
+                        
+                    }
+                    
+                    
+                } else{
+                    print("Erro ao realizar consulta de preco")
+                }
+            }
+            tarefa.resume()
+        }
+        
+    }
 
 }
 
